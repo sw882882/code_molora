fada79a168a54a41842d50ca754ef93608ffb60d
Andrew <seungwan.wee@gmail.com>
2023-09-14 21:02:32 +0700

    maybe fix

--- train_st_unsup.py
+++ train_st_unsup.py
@@ -1,47 +1,45 @@
 import math
 
-import nltk
 import pandas as pd
-from datasets import Dataset, DatasetDict, load_dataset
-from sentence_transformers import InputExample, SentenceTransformer, losses, models
-from sentence_transformers.datasets import DenoisingAutoEncoderDataset
-from sentence_transformers.losses import DenoisingAutoEncoderLoss
+from sentence_transformers import SentenceTransformer, LoggingHandler
+from sentence_transformers import models, util, datasets, evaluation, losses
 from torch.utils.data import DataLoader
+
 
 nltk.download("punkt")
 
 # load data
 df = pd.read_json("./evol-codealpaca-v1/train.jsonl", lines=True)
+# Define your sentence transformer model using CLS pooling
+model_name = "distilroberta-base"
+word_embedding_model = models.Transformer(model_name)
+pooling_model = models.Pooling(
+    word_embedding_model.get_word_embedding_dimension(), "cls"
+)
+model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
-dataset = DatasetDict(
-    {
-        "train": Dataset.from_pandas(df),
-    }
+# Define a list with sentences (1k - 100k sentences)
+train_sentences = df["instruction"].tolist()
+
+# Create the special denoising dataset that adds noise on-the-fly
+train_dataset = datasets.DenoisingAutoEncoderDataset(train_sentences)
+
+# DataLoader to batch your data
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+# Use the denoising auto-encoder loss
+train_loss = losses.DenoisingAutoEncoderLoss(
+    model, decoder_name_or_path=model_name, tie_encoder_decoder=True
 )
 
-print(dataset)
-
-model = models.Transformer("distilroberta-base")
-
-train_data = DenoisingAutoEncoderDataset(df["instruction"].tolist())
-loader = DataLoader(train_data, batch_size=128, shuffle=True, drop_last=True)
-
-pooling = models.Pooling(model.get_word_embedding_dimension(), "cls")
-model = SentenceTransformer(modules=[model, pooling])
-
-loss = DenoisingAutoEncoderLoss(model, tie_encoder_decoder=True)
-# round up to ensure all examples are seen
-num_epoch = math.ceil(len(df) / 64 * 2)
-
+# Call the fit method
 model.fit(
-    train_objectives=[(loader, loss)],
-    epochs=num_epoch,
+    train_objectives=[(train_dataloader, train_loss)],
+    epochs=1,
     weight_decay=0,
     scheduler="constantlr",
     optimizer_params={"lr": 3e-5},
     show_progress_bar=True,
 )
-# save model
-model.save("./unsupervised_fine_tune")
 
-model.push_to_hub("bert_code_instruct")
+model.save("output/tsdae-model")
